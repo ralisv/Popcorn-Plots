71c393637eb940875d70104245b4876eb909b097
Tomáš Režňák <tomas.reznak@volny.cz>
2026-01-10 22:12:35 +0100

    Filter data based on common occurence of ratings and movies

--- data-pipeline/src/__main__.py
+++ data-pipeline/src/__main__.py
@@ -27,19 +27,35 @@ def main() -> None:
     print("Loading MovieLens 32M dataset...")
     ml32m = load_movie_lens_32m()
 
-    print("Postprocessing MovieLens 32M dataset...")
-    ratings = postprocess_movielens(ml32m)
-
-    print("Writing MovieLens 32M dataset to parquet...")
-    _write_parquet(ratings, OUT_DIR / "ratings.parquet")
-
     print("Loading IMDb dataset...")
     imdb = load_imdb()
+
+    print("Postprocessing MovieLens 32M dataset...")
+    ratings = postprocess_movielens(ml32m)
 
     print("Postprocessing IMDb dataset...")
     title_basics = postprocess_imdb(imdb)
 
-    print("Writing IMDb dataset to parquet...")
+    ratings_filtered = ratings.join(
+        title_basics.select(pl.col("id").alias("imdbId")).unique(),
+        on="imdbId",
+        how="semi",
+    )
+    print(f"Filtered ratings: {ratings.shape[0]} -> {ratings_filtered.shape[0]}")
+
+    title_basics_filtered = title_basics.join(
+        ratings_filtered.select(pl.col("imdbId").alias("id")).unique(),
+        on="id",
+        how="semi",
+    )
+    print(
+        f"Filtered title_basics: {title_basics.shape[0]} -> {title_basics_filtered.shape[0]}"
+    )
+
+    print("Writing ratings.parquet...")
+    _write_parquet(ratings, OUT_DIR / "ratings.parquet")
+
+    print("Writing title_basics.parquet...")
     _write_parquet(title_basics, OUT_DIR / "title_basics.parquet")
 
 
